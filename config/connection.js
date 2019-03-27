03fc6687543a16fc72976dad7ac1aaa31422118d
Fooji <jfujitsubo@yahoo.com>
2019-03-27 16:43:51 -0700

    fixed bug

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    port: process.env || 3306,
+    port: 3306,
     user: 'root',
     password: 'psp123!!',
     database: "burgers_db"
