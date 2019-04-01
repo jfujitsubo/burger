d2effdd6a5449bb4542755fbb21ad1349589d7ac
Fooji <jfujitsubo@yahoo.com>
2019-03-31 23:36:42 -0700

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -5,20 +5,20 @@ if (process.env.JAWSDB_URL) {
 
 } else {
     connection = mysql.createConnection ({
-        host: 'localhost',
-        user: 'root',
-        password: 'hacktheplanet',
-        database: 'todoagain_db'
+        host: 'g9fej9rujq0yt0cd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'rxc15h2zl7fd9me23',
+        password: 'vcin22cu99hisnlh',
+        database: 'eoxcatrp9omp64vf'
     });
 };
 
-var connection = mysql.createConnection({
+/*var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'psp123!!',
     database: "burgers_db"
-});
+});*/
 
 connection.connect(function(err) {
     if (err) {
